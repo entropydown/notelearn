eeb2b411b272718140f37503e142d6e2e2178cd4
Nina Blanson <nina.m.blanson@gmail.com>
2019-03-22 12:26:23 -0500

    ES6-ifying, updating text, separating into fns

--- notelearn.js
+++ notelearn.js
@@ -3,64 +3,72 @@ const $ = require('jquery');
 
 VF = Vex.Flow;
 
-// Create an SVG renderer and attach it to the DIV element named "boo".
-var div = document.getElementById("display");
-var renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
-
-// Configure the rendering context.
+const div = document.getElementById('display');
+const renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
 renderer.resize(100, 200);
-var context = renderer.getContext();
-context.setFont("Arial", 10, "").setBackgroundFillStyle("#eed");
+const context = renderer.getContext();
+context.setFont('Arial', 10, '').setBackgroundFillStyle('#eed');
 
-function generateAndDisplayNote() {
+// Clears the latest note from the staff.
+function clearStaff() {
+  context.svg.removeChild(context.svg.lastChild);
+}
+
+// Generates and displays the note on the staff.
+// May want to split this up later depending on what we want to do...
+function generateAndDisplayNote(clef) {
   const octave = Math.max(Math.floor(Math.random() * 6), 4);
   const noteIndex = Math.floor(Math.random() * 7);
-  const noteBag = ["a", "b", "c", "d", "e", "f", "g"];
+  const notesBag = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
 
-  const pickedNote = noteBag[noteIndex];
+  const pickedNote = notesBag[noteIndex];
   const finalNote = `${pickedNote}/${octave}`;
 
-  // Create a stave of width 400 at position 10, 40 on the canvas.
-  var stave = new VF.Stave(10, 40, 100);
-
-  // Add a clef and time signature.
-  stave.addClef("treble").addTimeSignature("1/4");
-
-  // Connect it to the rendering context and draw!
+  const stave = new VF.Stave(10, 40, 100);
+  stave.addClef('treble').addTimeSignature('1/4');
   stave.setContext(context).draw();
 
-  var notes = [
-    new VF.StaveNote({clef: "treble", keys: [finalNote], duration: "q"}),
+  const notes = [
+    new VF.StaveNote({clef: 'treble', keys: [finalNote], duration: 'q'}),
   ];
 
-  var voice = new VF.Voice({num_beats: 1, beat_value: 4});
+  const voice = new VF.Voice({num_beats: 1, beat_value: 4});
   voice.addTickables(notes);
 
-  var formatter = new VF.Formatter().joinVoices([voice]).format([voice]);
+  const formatter = new VF.Formatter().joinVoices([voice]).format([voice]);
 
   voice.setStave(stave);
   voice.draw(context, stave);
+
   return pickedNote;
 }
 
-var clef = "treble"
-var target = generateAndDisplayNote();
-$('#answer-confirmation').text('determine note');
+function setAnswerConfirmation(text) {
+  $('#answer-confirmation').text(text);
+}
 
-$("button").click(function(event) {
-  if (this.id === target) {
-    //alert('correct!');
-    $('#answer-confirmation').text('correct! try another:');
-    context.svg.removeChild(context.svg.lastChild);
-    target = generateAndDisplayNote();
-  } else if (this.id === "generate") {
-    context.svg.removeChild(context.svg.lastChild);
-    target = generateAndDisplayNote();
-    $('#answer-confirmation').text('determine note');
-  } else {
-    //alert('incorrect!');
-    $('#answer-confirmation').text('incorrect. try again!');
-  }
-  console.log(this.id);
+const clef = 'treble';
+const pickNoteMessage = 'Pick the note from the staff below:';
+var displayedNote = generateAndDisplayNote(clef);
+
+setAnswerConfirmation(pickNoteMessage)
+
+// Handles generating a new note for the staff
+$('#generate').click((event) => {
+  clearStaff();
+  displayedNote = generateAndDisplayNote(clef);
+  setAnswerConfirmation(pickNoteMessage);
 });
 
+// Handles button clicks from the list of answers
+$('#answer-chooser button').click((event) => {
+  const selectedAnswer = event.currentTarget.id;
+
+  if (selectedAnswer === displayedNote) {
+    setAnswerConfirmation('Correct! Try another:');
+    clearStaff();
+    displayedNote = generateAndDisplayNote(clef);
+  } else {
+    setAnswerConfirmation('Incorrect. Try again!');
+  }
+});
